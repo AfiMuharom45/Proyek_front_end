4e937c6b702adefae81be85e2acf8738833240b5
AlfiMuharom <Alfimuharom65@gmail.com>
2023-06-04 18:51:44 +0700

    update3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,10 @@ function Navbar() {
     <nav className="navbar">
       <ul className="navbar__list">
         <li className="navbar__item">
-          <a className="navbar__link" href="/">Home</a>
+          <a className="navbar__link" href="/">Tambah Makanan</a>
         </li>
         <li className="navbar__item">
-          <a className="navbar__link" href="/home">Tambah Makanan</a>
+          <a className="navbar__link" href="/home">Menu</a>
         </li>
       </ul>
     </nav>
