ad4b80f33afb2ef684351f0c2eec3dc6b09f8770
AlfiMuharom <Alfimuharom65@gmail.com>
2023-06-04 16:53:46 +0700

    uptade

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route,  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductList from './components/producList';
 import Home from './components/home';
 
 function App() {
-  const [products, setProducts] = useState([]);
+  const [setProducts] = useState([]);
 
   const addProduct = (product) => {
-    setProducts([...products, product]);
+    setProducts((prevProducts) => [...prevProducts, product]);
   };
 
   return (
@@ -17,8 +17,8 @@ function App() {
       <Header />
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/productList' element={<ProductList addProduct={addProduct} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/productList" element={<ProductList addProduct={addProduct} />} />
       </Routes>
     </BrowserRouter>
   );
